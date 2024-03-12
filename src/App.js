6960eff7ceaccea311cfc9649614dd9bb36ddb60
KabirAguilar <aaguilar@krugerschool.edu.ec>
2024-03-11 20:29:29 -0500

    create Flats, FlatForm and AddFlats page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
+import AddFlats from './Pages/AddFlats';
+import Flats from './Pages/Flats';
 
 function App() {
   return (
     <Routes>
       <Route path={"/"} element={<Login/>}/>
       <Route path={"/dashboard"} element={<Home/>}/>
+      <Route path={"/flats"} element={<Flats/>}/>
+      <Route path={"/flats/new"} element={<AddFlats/>}/>
     </Routes>
     );
 }
---
+++ src/Pages/AddFlats.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FlatForm from "../Components/FlatForm";
+
+export default function AddFlats() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold mb-8">Add Flats</h1>
+      <FlatForm />
+    </div>
+  );
+}
